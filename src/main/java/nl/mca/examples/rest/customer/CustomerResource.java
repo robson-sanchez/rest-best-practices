f986580c4d74e3b1f05b8476dea604db1857be90
Robson Sanchez <robsonvinicius@gmail.com>
2018-11-06 00:27:22 +0100

    HTTP verbs

--- src/main/java/nl/mca/examples/rest/customer/CustomerResource.java
+++ src/main/java/nl/mca/examples/rest/customer/CustomerResource.java
@@ -1,28 +1,73 @@
 package nl.mca.examples.rest.customer;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Collection;
 
 @RestController
 public class CustomerResource {
 
-    @RequestMapping("/customers")
-    public ResponseEntity<List<Customer>> listCustomers() {
-        return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
+    private final CustomerService customerService;
+
+    public CustomerResource(CustomerService customerService) {
+        this.customerService = customerService;
     }
 
-    @RequestMapping("/customers/{id}")
+    @GetMapping("/customers")
+    public ResponseEntity<Collection<Customer>> listCustomers() {
+        Collection<Customer> customers = customerService.list(0, Integer.MAX_VALUE);
+
+        if (customers.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(customers);
+    }
+
+    @PostMapping("/customers")
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        return ResponseEntity.ok(customerService.create(customer));
+    }
+
+    @GetMapping("/customers/{id}")
     public ResponseEntity<Customer> findCustomer(@PathVariable("id") Long pk) {
-        Customer customer = new Customer();
-        customer.setPk(pk);
-
-        return new ResponseEntity<>(customer, HttpStatus.OK);
+        return ResponseEntity.ok(customerService.find(pk));
     }
 
+    @PutMapping("/customers/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("id") Long pk, @RequestBody Customer customer) {
+        customer.setPk(pk);
+        return ResponseEntity.ok(customerService.update(customer));
+    }
+
+    @PatchMapping("/customers/{id}")
+    public ResponseEntity<Customer> updateCustomerPartially(@PathVariable("id") Long pk, @RequestBody Customer customer) {
+        Customer existingCustomer = customerService.find(pk);
+        existingCustomer.setPk(pk);
+
+        if (customer.getEmail() != null) {
+            existingCustomer.setEmail(customer.getEmail());
+        }
+
+        if (customer.getFirstName() != null) {
+            existingCustomer.setFirstName(customer.getFirstName());
+        }
+
+        if (customer.getLastName() != null) {
+            existingCustomer.setLastName(customer.getLastName());
+        }
+
+        if (customer.getPhoneNumber() != null) {
+            existingCustomer.setPhoneNumber(customer.getPhoneNumber());
+        }
+
+        return ResponseEntity.ok(customerService.update(existingCustomer));
+    }
+
+    @DeleteMapping("/customers/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("id") Long pk) {
+        customerService.delete(pk);
+        return ResponseEntity.ok().build();
+    }
 }
